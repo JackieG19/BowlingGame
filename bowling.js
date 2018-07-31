7f14a031f19685dfc065e57a10f7869e25b06560
Jackie <JGtheophila19@gmail.com>
2018-07-31 10:31:02 -0400

    Create bowling.js

---
+++ bowling.js
@@ -0,0 +1,31 @@
+var bowling = function(){
+    this.rolls = [];
+};
+
+function bowling(pins){
+    this.rolling.push(pins);
+    
+};
+
+function score(){
+    var result = 0;
+    var throws = 0;
+    var game = this;
+    
+    for(var frame = 0; frame<10; frame++){
+        if(isSpare()){
+            result+=
+        }
+        
+        else{
+            result += this.rolls[throws] + this.rolls[throws + 1];
+        }
+        throw += 2;
+    }
+    
+    return result; 
+};
+
+function isSpare(){
+    
+};
